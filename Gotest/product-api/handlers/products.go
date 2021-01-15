10838dd929ffbc73356e2dd1100de1accc179f41
Kostmic <44363586+Kostmic@users.noreply.github.com>
2021-01-15 13:13:16 +0100

    Upload code

--- Gotest/product-api/handlers/products.go
+++ Gotest/product-api/handlers/products.go
@@ -15,10 +15,19 @@ func NewProducts(logger *log.Logger) *Products {
 	return &Products{logger}
 }
 
-func (product *Products) ServeHTTP(responseWriter http.ResponseWriter, reader *http.Request) {
+func (products *Products) ServeHTTP(responseWriter http.ResponseWriter, reader *http.Request) {
+	if reader.Method == http.MethodGet {
+		products.getProducts(responseWriter, reader)
+		return
+	}
+
+	//catch all
+	responseWriter.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (products *Products) getProducts(responseWriter http.ResponseWriter, reader *http.Request) {
 	productList := data.GetProducts()
 	err := productList.ToJSON(responseWriter)
-
 	if err != nil {
 		http.Error(responseWriter, "Unable to marshal json", http.StatusInternalServerError)
 	}
